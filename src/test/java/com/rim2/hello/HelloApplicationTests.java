22395cfdd1554dfd117bc535a90b5b2b4b08eeef
Rim-2 <turtle2260@gmail.com>
2025-06-25 14:41:17 +0900

    first commit

---
+++ src/test/java/com/rim2/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rim2.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
